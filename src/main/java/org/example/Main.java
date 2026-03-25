6c90fd8b74476260cafd33d67dd37f3724bdc3d8
Unai75 <unai.crupas@educa.jcyl.es>
2026-03-25 12:57:56 +0100

    primer commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword())) {
+            System.out.println("Conectado con suceso!");
+
+
+            //Creamos la sentencia sql que es para ordenar los empleados por su salario de forma descedente
+            String sql = "SELECT NOMBRE, SALARIO FROM EMPLEADO" +
+                    " ORDER BY SALARIO DESC ";
+            //La ejecutamos
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String nombre = (rs.getString("nombre"));
+                double salario = (rs.getInt("salario"));
+                System.out.println(nombre+"  "+salario);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
